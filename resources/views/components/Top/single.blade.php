343847179f4034ad3d4e084cdf91e16731bde06a
Tomasz Tomżyński <tomasztomzynski@MacBook-Pro-Tomasz.local>
2019-03-06 23:57:26 +0100

    test top

--- resources/views/components/Top/single.blade.php
+++ resources/views/components/Top/single.blade.php
@@ -1,8 +1,15 @@
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+{{--<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">--}}
 
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block w-100" src={{'img/main1.png'}} alt="FirstSlide">
-        </div>
+    {{--<div class="carousel-inner">--}}
+        {{--<div class="carousel-item active">--}}
+            {{--<img class="d-block w-100" src={{'img/bg.png'}} alt="FirstSlide">--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+
+<div class="jumbotron jumbotron-fluid" style="background: none; background-image: url({{'img/bg.png'}}); background-repeat: no-repeat; background-size: contain;  ">
+    <div class="container" style="height: 350px">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
 </div>
