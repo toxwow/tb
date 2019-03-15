1acee18ab800005352c5811e0e7b79cb117fe326
Tomasz Tomżyński <tomasztomzynski@MacBook-Pro-Tomasz.local>
2019-03-15 01:39:10 +0100

    fix error auth

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Photo;
 use App\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GalleryController extends Controller
 {
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Analytics;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Analytics\Period;
 
 class HomeController extends Controller
